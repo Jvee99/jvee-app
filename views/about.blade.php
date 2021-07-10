dea7dbbd4dfed877e405dbaf25e140ff3f256864
JVEE L. AGUINALDO <70934667+Jvee99@users.noreply.github.com>
2021-07-10 09:09:03 +0800

    Add files via upload

---
+++ views/about.blade.php
@@ -0,0 +1,30 @@
+<x-ap-layout>
+
+<section class="text-gray-600 body-font">
+    <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+      
+      <h2 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">HELLO!</h2>
+      <br></br>
+      <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">We are Jvee's Photography
+
+         We are A photographer, a professional that focuses on the art of taking photographs with a digital or film camera. Photographers use artificial and/or natural lighting to snap pictures of various people, places and things in a variety of settings. We are expert in different style of position in taking pictures.
+<br></br>
+          We are the photographer in the part of the Ilocos Norte, We give a better shots.
+<br></br>
+         The different styles of photography we know</p>
+
+         <ol>
+            <li>Spots Photography</li>
+            <li>Portrait Photography</li>
+            <li>Fashion Photography</li>
+            <li>Still Life Photography</li>
+            <li>Architctural Photography</li>
+         </ol>
+      
+    </div>
+</section>
+
+
+
+</x-ap-layout>
---
+++ views/contact.blade.php
@@ -0,0 +1,19 @@
+<x-ap-layout>
+
+  <section class="text-gray-600 body-font">
+    <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-red-900">CONTACT INFORMATION</h1>
+      <br></br>
+      <h2 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-black-900">Name:     Jvee Aguinaldo</h2>
+      <h2 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-black-900">CP #:     09509494786</h2>
+      <h2 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-black-900">Address:  Vintar, Ilocos Norte</h2>
+      <h2 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-black-900">Email:    aguinaldojvee@gmail.com</h2>
+      <br></br>
+      
+    </div>
+</section>
+
+
+
+</x-ap-layout>
